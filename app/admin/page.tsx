60c12570d8acd1ce9b53e04bc93a0b80d659943e
jasmynejean-remy <jasmynej14@gmail.com>
2025-07-16 18:06:47 -0400

    title

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,6 +1,14 @@
+import React from "react";
+
+
+export const metadata = {
+    title: 'Ariventures Admin',
+};
+
 export default function AdminDashHome() {
     return (
         <div>
+
             <h1>Dashboard</h1>
         </div>
     )
