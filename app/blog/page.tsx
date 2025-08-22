c2dc2709dee3ee64202d2dfdcbe57bec62fd2423
jasmynejean-remy <jasmynej14@gmail.com>
2025-08-21 23:13:44 -0400

    search functionality

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,21 +1,36 @@
 'use client'
-import {useEffect, useState} from "react";
+import React, {useEffect, useState} from "react";
 import layout from "@/styles/layout.module.css"
 import text from "@/styles/typography.module.css"
 import blog from "@/styles/blog.module.css"
+import form from "@/styles/forms.module.css"
+import buttons from "@/styles/buttons.module.css"
 import pageContent from '@/data/content/blog.json'
 import WpBlogCard from "@/components/WpBlogCard";
 import {fetchWpBlogPosts} from "@/lib/blogFunctions";
 import {CategoryNode, TagNode, PostNode} from "@/types";
+import {
+    InputMaybe,
+    OrderEnum,
+    PostObjectsConnectionOrderbyEnum,
+    PostStatusEnum,
+    RootQueryToPostConnectionWhereArgs
+} from "@/wordpress/gql/graphql";
 
 
 export default function AllBlogPosts() {
     const [posts, setPosts] = useState<PostNode[]>([])
     const [categories, setCategories] = useState<CategoryNode[]>([])
     const [tags, setTags] = useState<TagNode[]>([])
+    const [searchQuery, setSearchQuery] = useState("")
     const vars = {first: 11}
+    const where: RootQueryToPostConnectionWhereArgs = {
+        status: 'PUBLISH' as PostStatusEnum,
+        orderby: [{ field: 'DATE' as PostObjectsConnectionOrderbyEnum, order: 'DESC' as OrderEnum }],
+        categoryNotIn:["dGVybTo1Nw=="] as InputMaybe<string[]>
+    }
     useEffect(() => {
-        fetchWpBlogPosts(vars.first, '').then((data) => {
+        fetchWpBlogPosts(vars.first, '', where).then((data) => {
             console.log(data)
             setPosts(data.posts?.nodes ?? [])
             setCategories((data.categories?.nodes ?? []).filter(Boolean) as CategoryNode[])
@@ -23,6 +38,20 @@ export default function AllBlogPosts() {
          }
         )
     }, []);
+
+    const search = (e: React.FormEvent) => {
+        e.preventDefault()
+        console.log(searchQuery)
+        const whereSearch: RootQueryToPostConnectionWhereArgs = {
+            status: 'PUBLISH' as PostStatusEnum,
+            orderby: [{ field: 'DATE' as PostObjectsConnectionOrderbyEnum, order: 'DESC' as OrderEnum }],
+            search: searchQuery
+        }
+        setPosts([])
+        fetchWpBlogPosts(vars.first, '', whereSearch).then((data) => {
+            setPosts(data.posts?.nodes ?? [])
+        })
+    }
 
     if (!posts || posts.length === 0 || !posts[0]) {
         return <div className={layout.section}><p>Loading...</p></div>;
@@ -53,7 +82,17 @@ export default function AllBlogPosts() {
 
                     <div className={blog.sideSections}>
                         <div className={blog.sidePanelSection}>
-                            <h3>Search</h3>
+                            <form onSubmit={(e) => search(e)}>
+                                <div className={form.row}>
+                                    <input
+                                        type="text"
+                                        placeholder="Search Articles..."
+                                        className={form.input}
+                                        onChange={(e)=> setSearchQuery(e.target.value)}/>
+                                    <button className={buttons.primarySmall}>Search</button>
+                                </div>
+
+                            </form>
                         </div>
                         <div className={blog.sidePanelSection}>
                             <h3>Categories</h3>
